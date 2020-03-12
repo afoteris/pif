50bfcf870736126fa8ad6977a93232b984ed8c13
Andrei Korzhyts <andreikorzhyts@gmail.com>
2020-03-12 04:55:51 +0300

    -- Инструменты сборки --

--- build.pl
+++ build.pl
@@ -1,10 +1,11 @@
 use strict;
 use warnings;
+use File::Copy;
 
-# Скопировать оригиналы ПИФ из их репозитория.
-use File::Copy; copy("../vc_pif/PIF.md", "original/PIF.md");
-use File::Copy; copy("../vc_pif/PIF_en.md", "original/PIF_en.md");
+# Копирование оригиналов ПИФ из их репозитория.
+copy("../vc_pif/PIF.md", "original/PIF.md");
+copy("../vc_pif/PIF_en.md", "original/PIF_en.md");
 
-# Создать HTML-версию содержимого оригиналов ПИФ.
-system('cmark -t html original/PIF.md > PIF_content.html');
-system('cmark -t html original/PIF_en.md > PIF_content_en.html');
+# Создание HTML-версий содержимого оригиналов ПИФ.
+system("cmark -t html original/PIF.md > PIF_content.html");
+system("cmark -t html original/PIF_en.md > PIF_content_en.html");
